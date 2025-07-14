5701d509d7214ab9f4cc865ba4eb53b07d890852
dblimma06 <152748715+dblimma06@users.noreply.github.com>
2025-07-13 21:52:31 -0300

    Adicionando arquivos

---
+++ script.js
@@ -0,0 +1,13 @@
+const botaogradiente = document.querySelector('.botao');
+
+botaogradiente.addEventListener('click', () => {
+       if (document.body.style.backgroundImage === 'linear-gradient(to right, lightgreen, darkgreen)') { 
+       document.body.style.backgroundImage = 'none';
+       document.body.style.backgroundColor = 'green';
+       botaogradiente.textContent = 'Com Gradiente';
+    }
+    else {
+         document.body.style.backgroundImage = 'linear-gradient(to right, lightgreen, darkgreen)';
+         botaogradiente.textContent = 'Sem Gradiente';
+    }
+})
